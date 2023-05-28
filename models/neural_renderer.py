435f1f2dbe7e30fcee1f3100cd633e16bc5b07bf
FabianvanStijn <33290039+FabianvanStijn@users.noreply.github.com>
2023-05-28 21:09:11 +0200

    Changed the code for the neural render
    Also added best_model.pt, removed chairs.pt

--- models/neural_renderer.py
+++ models/neural_renderer.py
@@ -307,9 +307,9 @@ def load_model(filename):
   "img_shape": [3, 128, 128],
   "channels_2d": [64, 64, 128, 128, 128, 128, 256, 256, 128, 128, 128],
   "strides_2d": [1, 1, 2, 1, 2, 1, 2, 1, -2, 1, 1],
-  "channels_3d": [32, 32, 128, 128, 128, 2, 2, 2],
+  "channels_3d": [32, 32, 128, 128, 128, 3, 3, 3],
   "strides_3d": [1, 1, 2, 1, 1, -2, 1, 1],
-  "temporal_channels": 40,
+  "temporal_channels": 20,
   "num_channels_projection": [512, 256, 256],
   "num_channels_inv_projection": [256, 512, 1024],
   "mode": "bilinear"
